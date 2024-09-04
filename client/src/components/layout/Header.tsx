b26423c2bd2e92643ee7e0ad017c2b0a276da3f7
wkdghdwns199 <wkdghdwns199@naver.com>
2024-09-04 23:59:53 +0900

    feat : 효과음 토글 버튼 기능 생성

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -5,7 +5,8 @@ import { toggleDarkMode } from "../../modules/darkSlice";
 import IconButton from "../button/IconButton";
 import RulesModal from "../modal/RulesModal";
 import { useRecoilState } from "recoil";
-import { rulesModalState } from "../../recoil/recoil";
+import { rulesModalState, soundEffectStatus } from "../../recoil/recoil";
+import { faTruckField } from "@fortawesome/free-solid-svg-icons";
 const { useEffect, useState, useRef } = React;
 
 type Props = {
@@ -27,6 +28,12 @@ const Header = ({ dark, toggleDarkMode }: ComponentProps) => {
       document.documentElement.classList.remove("dark");
     }
   }, [dark]);
+
+  const [playSoundEffect, setPlaySoundEffect] = useRecoilState(soundEffectStatus);
+  const handlePlaySoundEffect = () => {
+    console.log(!playSoundEffect)
+    setPlaySoundEffect(!playSoundEffect)
+  }
 
   const handlePlayMusic = () => {
     const audio = audioRef.current;
@@ -62,6 +69,25 @@ const Header = ({ dark, toggleDarkMode }: ComponentProps) => {
         <audio ref={audioRef} src='audio/main_theme.mp3' loop />
         {isChrome() ? (
           <>
+              <IconButton
+              className="z-50 mr-3"
+              size="md"
+              onClick={() => {
+                handlePlaySoundEffect();
+              }}
+            >
+              {playSoundEffect ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+              </svg>
+              
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                </svg>
+              )}
+            </IconButton>
+
             <IconButton
               className="z-50 mr-3"
               size="md"
@@ -109,6 +135,7 @@ const Header = ({ dark, toggleDarkMode }: ComponentProps) => {
                 </svg>
               )}
             </IconButton>
+            
 
             <RulesModal onRequestClose={closeModal}>
               <div className="text-light-text dark:text-dark-text flex justify-center items-center">
